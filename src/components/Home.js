61333758ee7844a90ceb07f25a55664b1c338320
Brennan Flood <flood.brennan@gmail.com>
2022-09-07 17:46:46 -0400

    added home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,8 +4,14 @@ import { Link } from "react-router-dom"
 const Home = (props) => {
     return (
         <div className="home">
-            <div className="home-index">
-                <Link to="/servers">Server List</Link>
+            <div className="home-index-container">
+                <h1 className="home-title">Choose a Destination, Gamer</h1>
+                <div className="home-index">
+                    <Link className="home-index-item" to="/servers"> 
+                        <i class="fa fa-solid fa-server"></i> 
+                        <p className="home-index-item-text">Server List</p>
+                    </Link>
+                </div>
             </div>
         </div>
     )
